f73aa8dc7f938447cd11dee8125e7218fd2833c8
samade747 <samad.e747@gmail.com>
2024-02-13 12:13:38 +0500

    1213pm

--- farawayproject/src/Components/ItemList/item.js
+++ farawayproject/src/Components/ItemList/item.js
@@ -3,28 +3,49 @@ import { useState } from "react"; // Importing the useState hook from React
 export const ItemList = ({ item, setItemsArr }) => { // Declaring a functional component called ItemList, which receives 'item' and 'setItemsArr' as props
 
     return(
-        <div style={{
+        <div style={{ // Returning JSX: a div element with inline styling
             display: "flex",
             flexWrap: "wrap",
             width: "50%",
             margin: "0px auto"
         }}>
         {
-            item?.map((singleItem, indx) => (
+            item?.map((singleItem, indx) => ( // Mapping over the 'item' array to render each individual item
               <Item 
-                ItemList={item}
-                setItemsArr={setItemsArr}
-
-
-
+                itemList={item} // Passing 'item' array as prop to each Item component
+                setItemsArr={setItemsArr} // Passing 'setItemsArr' function as prop to each Item component
+                samad={singleItem}
+                number={indx}
               />
                 
             ))
         }
-
-
-
-
         </div>
     )
+}
+
+const Item = ({ samad, number, setItemsArr, itemList}) => { // Declaring a functional component called Item, which receives 'haroon', 'number', 'setItemsArr', and 'itemList' as props
+  const packedHandler = (packingValue, itemNumber) =>{ // Defining a function called 'packedHandler' to handle checkbox state changes
+    const merayItems = [...itemList] // creatin a copy of the 'itemList' array
+    merayItems[number].packed = !packingValue // Toggling the 'packed' property of the item at the specified index
+    setItemsArr(merayItems) // Updating the state of the items array with the modified copy
+  }
+  return 
+  (
+    <div style={{
+       display: "flex",
+       gap: "10px"
+    }}>
+      <input type="checkbox" checked={samad.packed} onChange={() => packedHandler(samad.packed, number)} />
+      <p style={{
+        color: "#dbc69b",
+        textDecoration: samad.packed ? "line-through" : "none"
+      }}>
+      {
+        `${samad.quantity} ${samad.name}`
+      }
+      </p>
+      <p>X</p>
+    </div>
+  )
 }
