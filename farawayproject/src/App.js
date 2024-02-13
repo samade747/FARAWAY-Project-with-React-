657597da619dcae4e79d937c4b2f82b297411fc4
samade747 <samad.e747@gmail.com>
2024-02-13 13:39:22 +0500

    139pm 13-2-24

--- farawayproject/src/App.js
+++ farawayproject/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { Logo } from './Components/Logo/Logo';
 import { Form } from './Components/Form/Form';
+// import { PackagingList } from './Components/PackagingList/PackagingList';
+import { useState } from 'react';
 
 
 function App() {
+ 
+  const [itemsArr, setItemArr] = useState(
+    
+  )
+
+
+
+
   return (
     <>
     <Logo />
-    <Form />
+    <Form setItemArr={set} />
     </>
     
 
