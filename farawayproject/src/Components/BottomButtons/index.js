ed66e0fbb53b17784606334add6d05afa996ad3b
samade747 <samad.e747@gmail.com>
2024-02-16 12:16:36 +0500

    Update index.js

--- farawayproject/src/Components/BottomButtons/index.js
+++ farawayproject/src/Components/BottomButtons/index.js
@@ -1,21 +1,20 @@
 import React, { useState } from "react"
 
+
 export const BottomButtons = ({ itemsArr, setItemsArr }) => {
-
-    const clearAllItems = () => {
-        setItemsArr([])
-    }
-
     const handleSortChange = (e) => {
         const selectedOption = e.target.value;
 
     // Determinig which sorting option was selected
     switch (selectedOption) {
-        case "inputOrder": sortByInputOrder();
+        case "inputOrder":
+            sortByInputOrder();
             break;
-        case "description" : sortByDescription();
+        case "description":
+            sortByDescription();
             break;
-        case "packedStatus" : sortByPackdedStatus();
+        case "packedStatus":
+            sortByPackdedStatus();
             break;
         default: 
             break;   
@@ -23,36 +22,45 @@ export const BottomButtons = ({ itemsArr, setItemsArr }) => {
 };
 
 const sortByInputOrder = () => {
+    if(Array.isArray(itemsArr)){
+        console.log(itemsArr)
     // copying the array 
-    const sortedItems = [...itemsArr];
+    const sortedItems = [...itemsArr].sort((a, b) => a.inputOrder - b.inputOrder)
     // sorting the items based on input order
-    sortedItems.sort((a, b) => a.inputOrder - b.inputOrder)
     setItemsArr(sortedItems);
-    
+    }
 }
 
 const sortByDescription = () => {
+    if(Array.isArray(itemsArr)){
 // copying the array 
-    var sortedItems = [...itemsArr];
+    var sortedItems = [...itemsArr].sort((a, b) => a.name.localeCompare(b.name));
 // sort the items alphabitcaly 
-    sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+    
 setItemsArr(sortedItems)
+    }
+}
+const sortByPackdedStatus = () => {
+    if(Array.isArray(itemsArr)){
+    const sortedItems = [...itemsArr].sort((a, b) => (a.packed === b.packed) ? 0 : a.packed ? 1 : -1)
+      setItemsArr(sortedItems)
+    }
 }
 
-const sortByPackdedStatus = () => {
-    const sortedItems = [...itemsArr]
-    sortedItems.sort((a, b) => (a.packed === b.packed) ? 0 : a.packed ? 1 : -1)
-    setItemsArr(sortedItems)
+const clearAllItems = () => {
+        setItemsArr([])
 }
 
 return (
         <div>
-            <select onChange={handleSortChange}>
-                <option value="">sort By</option>
-                <option value="inputOrder">InputOrder</option>
-                <option value="description">Description</option>
-                <option value="packedStatus">Packed Status</option>
-            </select>
+            <div>
+                <select onChange={handleSortChange}>
+                    <option value="">sort By</option>
+                    <option value="inputOrder">InputOrder</option>
+                    <option value="description">Description</option>
+                    <option value="packedStatus">Packed Status</option>
+                </select>
+            </div>
             <button onClick={clearAllItems} style={{ backgroundColor: "#fde7aa" }}>Clear List</button>
         </div>
     )
